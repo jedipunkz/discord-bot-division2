c8f81eff4955ed0b490fe029e145ec5cb2d246e3
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2019-05-10 23:51:49 +0900

    grafana のコメント追加

--- discord-bot-division2-message.py
+++ discord-bot-division2-message.py
@@ -18,6 +18,15 @@ class Isac(discord.Client):
                     'こちらを見てくだしあ..!!!' +
                     'https://twitter.com/thedivisiongame'
             )
+        
+        if message.content.startswith('統計'):
+            await message.channel.send(
+                    'こちらを見てくだしあ ..!!!\n' +
+                    'ユーザ名 : nakama \n' +
+                    'パスワード : naka-ma \n' +
+                    'http://124.41.86.100:23001/d/uWfWtAmZk/division2?orgId=1&from=now%2Fd&to=now%2Fd'
+            )
+
         if message.content == "眠たい" \
                 or message.content == "眠い" \
                 or message.content == "ねむい":
